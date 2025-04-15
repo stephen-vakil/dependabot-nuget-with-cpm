a9f3a7b0e3d2f33e117492e33f16acaffd313769
Stephen Vakil <svakil@stackoverflow.com>
2025-04-15 14:50:28 -0500

    switch to cpm

---
+++ demoapp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.SqlClient;
+
+var con = new SqlConnection("Server=localhost;Database=master;User Id=sa;Password=your_password;");
+con.Open();
+
